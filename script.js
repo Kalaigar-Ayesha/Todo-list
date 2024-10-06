ac43a0c5422eb171eab7fe170df46368feae31c3
Kalaigar-Ayesha <ayesha389922@gmail.com>
2024-10-06 17:27:25 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,35 @@
+let btn = document.querySelector("button");
+let ul = document.querySelector("ul");
+let inp = document.querySelector("input");
+
+btn.addEventListener("click", function () {
+  let item = document.createElement("li");
+  item.innerText = inp.value;
+
+  let del = document.createElement("button");
+  del.innerText = "delete";
+  del.classList.add("delete");
+
+  item.appendChild(del);
+  ul.appendChild(item);
+
+  inp.value = "";
+});
+
+ul.addEventListener("click", function (event) {
+  //   console.log(event.target.nodeName);
+  //   console.log("obj clicked");
+  if (event.target.nodeName == "BUTTON") {
+    let listItem = event.target.parentElement;
+    listItem.remove();
+    console.log("deleted");
+  }
+});
+
+// let delBtn = document.querySelectorAll(".delete");
+// for (del of delBtn) {
+//   del.addEventListener("click", function () {
+//     let par = this.parentElement;
+//     par.remove();
+//   });
+// }
